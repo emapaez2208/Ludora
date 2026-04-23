2c2e14dfc74e8212721cf872646d983be31ef047
emapaez2208 <emapaezspognardi@gmail.com>
2026-04-23 10:39:38 -0300

    Initial commit

---
+++ src/main/java/ExperienceGroup/Ludora/LudoraApplication.java
@@ -0,0 +1,13 @@
+package ExperienceGroup.Ludora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ExperienceGroup/Ludora/LudoraApplicationTests.java
@@ -0,0 +1,13 @@
+package ExperienceGroup.Ludora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
