8cb03b51d1f83379c78a4abb55a07a689497ea18
Ferjs3 <101979907+Ferjs3@users.noreply.github.com>
2026-04-28 12:43:39 -0300

    Creacion SaleEntity

---
+++ src/main/java/ExperienceGroup/Ludora/features/sale/SaleEntity.java
@@ -0,0 +1,46 @@
+package ExperienceGroup.Ludora.features.sale;
+
+import ExperienceGroup.Ludora.features.game.GameEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table (name = "sale")
+public class SaleEntity{
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private double totalPrice = 0.0;
+
+    @ManyToOne
+    @JoinColumn (name = "client_id")
+    private ClientEntity client;
+
+    @ManyToMany
+    @JoinTable(
+            name = "sales_games",
+            joinColumns = @JoinColumn(name = "sale_id"),
+            inverseJoinColumns = @JoinColumn(name = "game_id")
+    )
+    private List<GameEntity> games;
+
+}
