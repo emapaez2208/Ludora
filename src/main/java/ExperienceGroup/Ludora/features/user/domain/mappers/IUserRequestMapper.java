e86058e99482c60548bd737a40475854d1115b24
emapaez2208 <emapaezspognardi@gmail.com>
2026-05-05 11:35:18 -0300

    delete imports inescesarios

--- src/main/java/ExperienceGroup/Ludora/features/user/domain/mappers/IUserRequestMapper.java
+++ src/main/java/ExperienceGroup/Ludora/features/user/domain/mappers/IUserRequestMapper.java
@@ -1,12 +1,9 @@
 package ExperienceGroup.Ludora.features.user.domain.mappers;
 
-import ExperienceGroup.Ludora.common.utils.Email;
 import ExperienceGroup.Ludora.common.utils.IMapper;
-import ExperienceGroup.Ludora.common.utils.Password;
 import ExperienceGroup.Ludora.features.user.domain.UserEntity;
 import ExperienceGroup.Ludora.features.user.domain.dto.UserDTORequest;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IUserRequestMapper extends IMapper<UserEntity, UserDTORequest> {
