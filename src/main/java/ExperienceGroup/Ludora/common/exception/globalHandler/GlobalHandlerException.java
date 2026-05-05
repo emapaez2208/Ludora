362547d69e971b12476566dc92af9d34e63ef092
emapaez2208 <emapaezspognardi@gmail.com>
2026-05-05 11:18:10 -0300

    Creacion excepcion de password

--- src/main/java/ExperienceGroup/Ludora/common/exception/globalHandler/GlobalHandlerException.java
+++ src/main/java/ExperienceGroup/Ludora/common/exception/globalHandler/GlobalHandlerException.java
@@ -1,5 +1,6 @@
-package ExperienceGroup.Ludora.common.exception;
+package ExperienceGroup.Ludora.common.exception.globalHandler;
 
+import ExperienceGroup.Ludora.common.exception.IllegalEmailException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
