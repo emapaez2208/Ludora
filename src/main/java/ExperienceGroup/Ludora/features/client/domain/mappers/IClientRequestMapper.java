eaed074519d1898612502edbcfa63154dbc6dc99
German <germaniyo181818@gmail.com>
2026-05-05 12:27:42 -0300

    Update IClientRequestMapper.java

--- src/main/java/ExperienceGroup/Ludora/features/client/domain/mappers/IClientRequestMapper.java
+++ src/main/java/ExperienceGroup/Ludora/features/client/domain/mappers/IClientRequestMapper.java
@@ -3,9 +3,10 @@ package ExperienceGroup.Ludora.features.client.domain.mappers;
 import ExperienceGroup.Ludora.common.utils.IMapper;
 import ExperienceGroup.Ludora.features.client.domain.ClientEntity;
 import ExperienceGroup.Ludora.features.client.domain.dto.ClientDTORequest;
+import ExperienceGroup.Ludora.features.user.domain.mappers.IUserRequestMapper;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring" , uses = {IUserRequestMapper.class})
 public interface IClientRequestMapper extends IMapper<ClientEntity,ClientDTORequest> {
     @Override
     ClientDTORequest toDTO(ClientEntity clientEntity) ;
