8c7bc3c8ca63605285fc137394bf4028a258aa78
emapaez2208 <emapaezspognardi@gmail.com>
2026-05-05 10:47:31 -0300

    modificacion mapper y dto
    Ahora los DTO manejan datos primitivos y los valido al mappear, cree la excepcion de Email para poder manejarla correctamente en el GlobalHandler

--- src/main/java/ExperienceGroup/Ludora/common/exception/GlobalHandlerException.java
+++ src/main/java/ExperienceGroup/Ludora/common/exception/GlobalHandlerException.java
@@ -1,4 +1,18 @@
 package ExperienceGroup.Ludora.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalHandlerException {
+
+    @ExceptionHandler(IllegalEmailException.class)
+    public ResponseEntity<String> HandleIllegalEmail(IllegalEmailException ex){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+
 }
--- src/main/java/ExperienceGroup/Ludora/features/user/domain/mappers/IUserRequestMapper.java
+++ src/main/java/ExperienceGroup/Ludora/features/user/domain/mappers/IUserRequestMapper.java
@@ -1,13 +1,29 @@
 package ExperienceGroup.Ludora.features.user.domain.mappers;
 
+import ExperienceGroup.Ludora.common.utils.Email;
 import ExperienceGroup.Ludora.common.utils.IMapper;
+import ExperienceGroup.Ludora.common.utils.Password;
 import ExperienceGroup.Ludora.features.user.domain.UserEntity;
 import ExperienceGroup.Ludora.features.user.domain.dto.UserDTORequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IUserRequestMapper extends IMapper<UserEntity, UserDTORequest> {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "externalId", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "statusBlocked", ignore = true)
+    @Mapping(target = "games", ignore = true)
     UserEntity toEntity(UserDTORequest userDTORequest);
     UserDTORequest toDTO(UserEntity userEntity);
+
+    default Email mapEmail(String email){
+        return email != null ? new Email(email) : null;
+    }
+
+    default Password mapPassword(String password){
+        return password != null ? new Password(password) : null;
+    }
 }
