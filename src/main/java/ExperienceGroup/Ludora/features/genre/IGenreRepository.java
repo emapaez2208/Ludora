c26f3a6322b5b57a09046e97a517c183f75f0f57
German <germaniyo181818@gmail.com>
2026-05-05 13:40:11 -0300

    Genre

--- src/main/java/ExperienceGroup/Ludora/features/genre/IGenreRepository.java
+++ src/main/java/ExperienceGroup/Ludora/features/genre/IGenreRepository.java
@@ -1,12 +1,13 @@
 package ExperienceGroup.Ludora.features.genre;
 
-import ExperienceGroup.Ludora.features.genre.domain.GenreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ExperienceGroup.Ludora.features.genre.domain.GenreEntity;
 
 import java.util.Optional;
+
 @Repository
-public interface IGenreRepository extends JpaRepository<GenreEntity,Long> {
+public interface IGenreRepository extends JpaRepository<GenreEntity, Long> {
     Optional<GenreEntity> findByName(String name);
 
 }
--- src/main/java/ExperienceGroup/Ludora/features/genre/domain/GenreEntity.java
+++ src/main/java/ExperienceGroup/Ludora/features/genre/domain/GenreEntity.java
@@ -4,6 +4,7 @@ import ExperienceGroup.Ludora.features.game.GameEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class GenreEntity {
 
     @Id
