d0975a6e26187a40c1f8fd34f72924e3a1aec407
German <germaniyo181818@gmail.com>
2026-05-05 10:11:12 -0300

    Clients
    hice ClientDTO , Mappers e iREpository

--- src/main/java/ExperienceGroup/Ludora/features/client/domain/dto/ClientDTORequest.java
+++ src/main/java/ExperienceGroup/Ludora/features/client/domain/dto/ClientDTORequest.java
@@ -1,14 +1,26 @@
 package ExperienceGroup.Ludora.features.client.domain.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
-public record ClientDTORequest (@Positive int phone ,
-                                @NotBlank
+import java.time.LocalDate;
 
-
+public record ClientDTORequest (@Schema(description = "Numero de telefono",example = "123" , required = true)
+                                @Size(min = 0 , max =15 , message = "El numero tiene q ten")
+                                @Positive (message = "No puede ser un numero negativo")
+                                @NotNull int phone ,
+                                @Schema(description = "Nombre de la calle", example = "Luro" , required = true)
+                                @Size(max = 25 , message = "Tiene que tener como maximo 25 caracteres")
+                                @NotNull String street,
+                                @Schema (description = "numero de la calle",example = "123",required = true)
+                                @Positive(message = "No puede ser numero negativo")
+                                @NotBlank int numberStreet,
+                                @Schema(description = "Fecha de nacimiento", example = "10/10/10", required = true)
+                                @NotNull LocalDate birhdate
                                 ){
 
 }
--- src/main/java/ExperienceGroup/Ludora/features/client/domain/dto/ClientDTOResponse.java
+++ src/main/java/ExperienceGroup/Ludora/features/client/domain/dto/ClientDTOResponse.java
@@ -1,25 +1,11 @@
 package ExperienceGroup.Ludora.features.client.domain.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class ClientDTOResponse {
-
-    private int phone;
-
-    private String street;
-
-
-    private int numberStreet;
-
-    private LocalDate birhDate;
+public record ClientDTOResponse ( int phone,
+                                  String street,
+                                  int numberStreet,
+                                   LocalDate birhDate)
+{
 
 }
