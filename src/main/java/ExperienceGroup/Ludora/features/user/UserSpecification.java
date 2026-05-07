175184ec5ee72639eef9ebe30047db7c80b989cf
emapaez2208 <emapaezspognardi@gmail.com>
2026-05-07 09:14:17 -0300

    terminacion Specification
    agregue los predicados para las busquedas.

--- src/main/java/ExperienceGroup/Ludora/features/user/UserSpecification.java
+++ src/main/java/ExperienceGroup/Ludora/features/user/UserSpecification.java
@@ -11,11 +11,45 @@ public class UserSpecification {
                 : cb.like(cb.lower(root.get("name")), "%" + name.toLowerCase() + "%");
     }
 
+    public static PredicateSpecification<UserEntity> nameEquals(String name){
+        return (root, cb) -> name == null || name.isBlank()
+                ? cb.conjunction()
+                : cb.equal(cb.lower(root.get("name")), name.toLowerCase());
+    }
+
     public static PredicateSpecification<UserEntity> lastNameContains(String lastName){
         return (root, cb) -> lastName == null || lastName.isBlank()
                 ? cb.conjunction()
                 : cb.like(cb.lower(root.get("lastName")), "%" + lastName.toLowerCase() + "%");
     }
 
+    public static PredicateSpecification<UserEntity> lastNameEquals(String lastName){
+        return (root, cb) -> lastName == null || lastName.isBlank()
+                ? cb.conjunction()
+                : cb.equal(cb.lower(root.get("lastName")), lastName.toLowerCase());
+    }
 
+    public static PredicateSpecification<UserEntity> userNameEquals(String userName){
+        return (root, cb) -> userName == null || userName.isBlank()
+                ? cb.conjunction()
+                : cb.equal(cb.lower(root.get("userName")), userName.toLowerCase());
+    }
+
+    public static PredicateSpecification<UserEntity> statusBlockedEquals(Boolean status){
+        return (root, cb) -> status == null
+                ? cb.conjunction()
+                : cb.equal(root.get("statusBlocked"), status);
+    }
+
+    public static PredicateSpecification<UserEntity> emailEquals(String email){
+        return (root, cb) -> email == null || email.isBlank()
+                ? cb.conjunction()
+                : cb.equal(cb.lower(root.get("email").get("value")), email.toLowerCase());
+    }
+
+    public static PredicateSpecification<UserEntity> roleEquals(String role){
+        return (root, cb) -> role == null || role.isBlank()
+                ? cb.conjunction()
+                : cb.equal(cb.lower(root.get("role").get("name")), role.toLowerCase());
+    }
 }
