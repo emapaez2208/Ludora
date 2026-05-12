aeae2b4f6c1be2545abcdfa4587f7c6bb8a95ef9
German <germaniyo181818@gmail.com>
2026-05-12 14:12:50 -0300

    CLientService
    falta controller

---
+++ src/main/java/ExperienceGroup/Ludora/features/client/ClientService.java
@@ -0,0 +1,84 @@
+package ExperienceGroup.Ludora.features.client;
+
+import ExperienceGroup.Ludora.common.exception.UserNotFoundException;
+import ExperienceGroup.Ludora.common.utils.IMapper;
+import ExperienceGroup.Ludora.features.client.domain.ClientEntity;
+import ExperienceGroup.Ludora.features.client.domain.dto.ClientDTORequest;
+import ExperienceGroup.Ludora.features.client.domain.dto.ClientDTOResponse;
+import ExperienceGroup.Ludora.features.user.UserSpecification;
+import ExperienceGroup.Ludora.features.user.domain.UserEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.domain.PredicateSpecification;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+@Service
+@AllArgsConstructor
+public class ClientService implements IClientService{
+    private IClientRepository repository;
+    private IMapper<ClientEntity,ClientDTOResponse> mapperResponse;
+    private IMapper<ClientEntity,ClientDTORequest> mapperRequest;
+
+    @Override
+    public List<ClientDTOResponse> getAllClient(String name,
+                                                String lastName,
+                                                String userName,
+                                                String email,
+                                                Boolean statusBlocked,
+                                                Integer phone,
+                                                String street,
+                                                Integer numberStreet,
+                                                LocalDate birthDate) {
+        PredicateSpecification<ClientEntity> spec = PredicateSpecification.allOf(
+                ClientSpecification.nameContains(name),
+                ClientSpecification.lastNameContains(lastName),
+                ClientSpecification.userNameEquals(userName),
+                ClientSpecification.emailEquals(email),
+                ClientSpecification.statusBlockedEquals(statusBlocked),
+                ClientSpecification.phoneEquals(phone),
+                ClientSpecification.streetEquals(street),
+                ClientSpecification.numberStreetEquals(numberStreet),
+                ClientSpecification.birthDateEquals(birthDate)
+        );
+
+        return repository.findAll(spec).stream()
+                .map(mapperResponse::toDTO)
+                .toList();
+    }
+
+    @Override
+    public ClientDTOResponse getByExternalID(UUID externalID) {
+        return repository.findByExternalId(externalID).stream()
+                .map(mapperResponse::toDTO)
+                .findFirst()
+                .orElseThrow(()->new UserNotFoundException("Client not found"));
+    }
+
+    @Override
+    public ClientDTOResponse getByUserName(String userName) {
+        return repository.findByUserName(userName).stream()
+                .map(mapperResponse::toDTO)
+                .findFirst()
+                .orElseThrow(()->new UserNotFoundException("Client not found"));
+    }
+
+    @Override
+    public ClientDTOResponse save(ClientDTORequest clientDTORequest) {
+
+        ClientEntity entity = mapperRequest.toEntity(clientDTORequest);
+        repository.save(entity);
+
+        return mapperResponse.toDTO(entity);
+    }
+
+    @Override
+    public void delete(UUID externalID) {
+        ClientEntity entityDelete = repository.findByExternalId(externalID).
+                orElseThrow(()->new UserNotFoundException ("Client not found"));
+
+        repository.delete(entityDelete);
+
+    }
+}
--- src/main/java/ExperienceGroup/Ludora/features/client/IClientRepository.java
+++ src/main/java/ExperienceGroup/Ludora/features/client/IClientRepository.java
@@ -1,12 +1,23 @@
 package ExperienceGroup.Ludora.features.client;
 
 import ExperienceGroup.Ludora.features.client.domain.ClientEntity;
+import ExperienceGroup.Ludora.features.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface IClientRepository extends JpaRepository<ClientEntity,Long> {
+public interface IClientRepository extends JpaRepository<ClientEntity,Long>, JpaSpecificationExecutor<ClientEntity> {
+    Optional<ClientEntity> findByUserName(String userName);
+
+    Optional<ClientEntity> findByExternalId(UUID externalId);
+
+    Optional<ClientEntity> findByNameAndLastName(String name, String lastName);
+
+    Optional<ClientEntity> findByStatusBlocked(Boolean statusBlocked);
     
 }
