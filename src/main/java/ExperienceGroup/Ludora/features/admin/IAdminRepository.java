2eed08a75cf2fd62963a91dba17ddcd3eff5a677
emapaez2208 <emapaezspognardi@gmail.com>
2026-05-12 12:30:49 -0300

    Terminacion Service y Repositorio

--- src/main/java/ExperienceGroup/Ludora/features/admin/IAdminRepository.java
+++ src/main/java/ExperienceGroup/Ludora/features/admin/IAdminRepository.java
@@ -1,14 +1,21 @@
 package ExperienceGroup.Ludora.features.admin;
 
 import ExperienceGroup.Ludora.features.admin.domain.AdminEntity;
-import ExperienceGroup.Ludora.features.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface IAdminRepository extends JpaRepository<AdminEntity, Long> {
+public interface IAdminRepository extends JpaRepository<AdminEntity, Long>, JpaSpecificationExecutor<AdminEntity> {
 
     Optional<AdminEntity> findByEmployeeId (Long employeeId);
 
+    Optional<AdminEntity> findByUserName(String userName);
 
+    Optional<AdminEntity> findByExternalId(UUID externalId);
+
+    Optional<AdminEntity> findByNameAndLastName(String name, String lastName);
+
+    Optional<AdminEntity> findByStatusBlocked(Boolean statusBlocked);
 }
--- src/main/java/ExperienceGroup/Ludora/features/admin/IAdminService.java
+++ src/main/java/ExperienceGroup/Ludora/features/admin/IAdminService.java
@@ -5,18 +5,24 @@ import ExperienceGroup.Ludora.features.admin.domain.dto.AdminDTOResponse;
 import ExperienceGroup.Ludora.features.user.domain.dto.UserDTOResponse;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface IAdminService {
 
     List<AdminDTOResponse> getAllAdmins(String name,
                                         String lastName,
                                         String userName,
-                                        String role,
                                         String email,
                                         Boolean statusBlocked,
                                         Long employeeId);
 
+    AdminDTOResponse getByExternalId(UUID externalId);
+
+    AdminDTOResponse getByUserName(String userName);
+
+    AdminDTOResponse getByEmployeeId(Long employeeId);
+
     AdminDTOResponse save(AdminDTORequest adminDTO);
 
-    void delete(Long employeeId);
+    void delete(UUID externalId);
 }
