ab1be5c172a94a6ed493d777cb8798867d1fc70d
DESIMONE-DEV <desimone.oscar.ale@gmail.com>
2026-05-13 12:49:14 -0300

    Developer
    Mapper/ Listos
    DTO / Listos
    IRepository / Listo
    DeveloperSpecification/Listo
    IService/Listo
    Service / Listo (Acepto sugerencia de algun otro metodo faltante)

---
+++ src/main/java/ExperienceGroup/Ludora/features/developer/IDeveloperService.java
@@ -0,0 +1,29 @@
+package ExperienceGroup.Ludora.features.developer;
+
+import ExperienceGroup.Ludora.features.developer.domain.dto.DeveloperDtoRequest;
+import ExperienceGroup.Ludora.features.developer.domain.dto.DeveloperDtoResponse;
+import ExperienceGroup.Ludora.features.game.domain.dto.GameDTOResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IDeveloperService {
+
+    List<DeveloperDtoResponse> getAllDevelopers(String name,
+                                                String lastName,
+                                                String userName,
+                                                String role,
+                                                String email,
+                                                Boolean statusBlocked,
+                                                String company);
+
+    DeveloperDtoResponse getByExternalId(UUID externalId);
+
+    DeveloperDtoResponse save(DeveloperDtoRequest developerDtoRequest);
+
+    DeveloperDtoResponse update(UUID externalId, DeveloperDtoRequest developerDtoRequest);
+
+    List<GameDTOResponse> getGamesByDeveloper(UUID externalId);
+
+    void delete(UUID externalId);
+}
