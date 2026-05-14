55e809e62c7481540bc6be72c80a175c7dc640ca
Ferjs3 <101979907+Ferjs3@users.noreply.github.com>
2026-05-14 12:25:28 -0300

    Comienzo ReviewSpecification

---
+++ src/main/java/ExperienceGroup/Ludora/features/review/ReviewSpecification.java
@@ -0,0 +1,9 @@
+package ExperienceGroup.Ludora.features.review;
+
+import ExperienceGroup.Ludora.features.review.domain.ReviewEntity;
+import org.springframework.data.jpa.domain.PredicateSpecification;
+
+public class ReviewSpecification {
+
+    public static PredicateSpecification<ReviewEntity>
+}
