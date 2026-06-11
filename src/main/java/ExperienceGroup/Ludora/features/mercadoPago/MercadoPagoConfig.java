7261b2ddf807c8e4b34a17f119c0c6ab35c1a3d3
emapaez2208 <emapaezspognardi@gmail.com>
2026-06-11 09:20:35 -0300

    agrego dependencia de mercado pago

---
+++ src/main/java/ExperienceGroup/Ludora/features/mercadoPago/MercadoPagoConfig.java
@@ -0,0 +1,18 @@
+package ExperienceGroup.Ludora.features.mercadoPago;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MercadoPagoConfig {
+
+    @Value("${mercadopago.access-token}")
+    private String accessToken;
+
+
+    @PostConstruct
+    public void init(){
+        com.mercadopago.MercadoPagoConfig.setAccessToken(accessToken);
+    }
+}
