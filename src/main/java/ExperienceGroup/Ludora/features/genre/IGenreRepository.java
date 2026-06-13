084df20a7e2e72f014f53c9cac3889815f2f3b9e
German <germaniyo181818@gmail.com>
2026-06-13 12:12:45 -0300

    listaDeReview

--- src/main/java/ExperienceGroup/Ludora/features/genre/IGenreRepository.java
+++ src/main/java/ExperienceGroup/Ludora/features/genre/IGenreRepository.java
@@ -12,6 +12,4 @@ public interface IGenreRepository extends JpaRepository<GenreEntity,Long> {
 
     Optional<GenreEntity> findByDescription(String description);
 
-    Optional<List<GenreEntity>> findAllByName(List<String> name);
-
 }
