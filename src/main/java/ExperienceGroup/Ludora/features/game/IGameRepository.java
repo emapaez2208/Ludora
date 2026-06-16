dbe1b1a808c62eb5a87e54f6266d786d2d8290be
DESIMONE-DEV <desimone.oscar.ale@gmail.com>
2026-06-16 02:13:29 -0300

    comprobacion

--- src/main/java/ExperienceGroup/Ludora/features/game/IGameRepository.java
+++ src/main/java/ExperienceGroup/Ludora/features/game/IGameRepository.java
@@ -1,6 +1,7 @@
 package ExperienceGroup.Ludora.features.game;
 
 import ExperienceGroup.Ludora.features.game.domain.GameEntity;
+import ExperienceGroup.Ludora.features.genre.domain.GenreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -31,5 +32,7 @@ public interface IGameRepository extends JpaRepository<GameEntity, Long>, JpaSpe
     List<GameEntity> findByReleaseDateBetween(LocalDate minDate, LocalDate maxDate);
 
     Optional<List<GameEntity>> findAllByNeedRevision(Boolean needRevision);
+
+    boolean existsByGenresContaining(GenreEntity genre);
 
 }
