c7abad3a148263ab3de7a37bf40972a05faea4e5
Ferjs3 <fernandojs120@gmail.com>
2026-06-16 02:13:07 -0300

    Creacion endpoint Enviar juego a revisión  (developer) y metodo de ver dichos juegos (admin)(

--- src/main/java/ExperienceGroup/Ludora/features/game/GameService.java
+++ src/main/java/ExperienceGroup/Ludora/features/game/GameService.java
@@ -200,10 +200,9 @@ public class GameService implements IGameService{
     @Override
     @PreAuthorize("hasRole('ADMIN')")
     public List<GameDTOResponse> getGamesNeedRevision(){
-         List<GameEntity> blockedGames = gameRepository.findByStatusBlocked(true);
-
-        return blockedGames.stream()
-                .map(responseMapper::toDTO)
+        return gameRepository.findAllByNeedRevision(true)
+                .orElseThrow(GameNotFoundException::new)
+                .stream().map(responseMapper::toDTO)
                 .toList();
     }
     /// -------------- DEVELOPER SOLICITAR REVISION DE UN JUEGO ---------------------
